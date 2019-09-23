41f2f1605ad79b0114199ce104ba845422433fe6
Bartosz Bielicki <bartosz.bielicki@gmail.com>
2019-09-23 19:35:03 +0200

    Dockerize application

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = process.env.REACT_APP_API_ROOT || "http://backend:5000/api"
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
